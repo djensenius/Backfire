408a2f50f0136f76e2f5a458ba1cc130d41230e0
David Jensenius <david@jensenius.com>
2021-04-05 21:27:09 -0400

    Initial Commit

---
+++ Shared/BackfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackfireApp.swift
+//  Shared
+//
+//  Created by David Jensenius on 2021-04-05.
+//
+
+import SwiftUI
+
+@main
+struct BackfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
